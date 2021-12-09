0370c79fa6d67abc392fc92c318d4ac12a70bde4
Jessica Aidyl <jgarciaalbalah@gmail.com>
2021-12-08 21:00:05 -0500

    + Widget Constants

---
+++ lib/src/ui/f_widget/widget_constants.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+//// Trivia Single Level Screen Constants///////
+
+const triviaLevelPrimaryColor = Color(0xFF46A0AE);
+const triviaLevelSecundaryColor = Color(0xFF00FFCB);
+const triviaLevelDarkColor = const Color(0xFF3F4768);
+
+const progressBarGradient = LinearGradient(
+  colors: [
+    Color(0xFF46A0AE),
+    Color(0xFF00FFCB),
+  ],
+  begin: Alignment.centerLeft,
+  end: Alignment.centerRight,
+);
+
+const kSecondaryColor = Color(0xFF8B94BC);
+const kGreenColor = Color(0xFF6AC259);
+const kRedColor = Color(0xFFE92E30);
+const kGrayColor = Color(0xFFC1C1C1);
+const kBlackColor = Color(0xFF101010);
+
+const double kDefaultPadding = 20.0;
