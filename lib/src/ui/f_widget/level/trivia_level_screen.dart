c9768bf99c66b8a2ea5ad4714c76f15c6920f8ad
Jessica Aidyl <jgarciaalbalah@gmail.com>
2021-12-08 21:04:11 -0500

    TriviaLevelScreen: +Stepper Widget

--- lib/src/ui/f_widget/level/trivia_level_screen.dart
+++ lib/src/ui/f_widget/level/trivia_level_screen.dart
@@ -1,17 +1,32 @@
+import 'package:citmatel_strawberry_trivia/trivia_exporter.dart';
+import 'package:citmatel_strawberry_trivia/src/ui/f_widget/widget_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_countdown_timer/index.dart';
-import 'package:get/get.dart';
+
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:im_stepper/stepper.dart';
 import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
 
-import 'package:citmatel_strawberry_trivia/src/ui/b_controller/level_controller.dart';
-
 // ignore: must_be_immutable
-class TriviaLevelScreen extends GetView<LevelController> {
+class TriviaLevelScreen extends StatefulWidget {
   static const ROUTE_NAME = "/trivia-level-screen";
-  late CountdownTimerController _timerController;
 
-  TriviaLevelScreen({Key? key}) : super(key: key);
+  late SubLevelController subLevelController;
+
+  TriviaLevelScreen({
+    required SubLevelDomain subLevelDomain,
+    Key? key,
+  })  : subLevelController =
+            SubLevelControllerImpl(subLevelDomain: subLevelDomain),
+        super(key: key);
+
+  @override
+  State<TriviaLevelScreen> createState() => _TriviaLevelScreenState();
+}
+
+class _TriviaLevelScreenState extends State<TriviaLevelScreen> {
+  late CountdownTimerController _timerController;
 
   _buildCountDown() {
     int startTime = DateTime.now().millisecondsSinceEpoch;
@@ -32,15 +47,15 @@ class TriviaLevelScreen extends GetView<LevelController> {
         print("perc");
         return Stack(children: [
           Container(
-            padding: EdgeInsets.all(15),
-            height: 80,
+            padding: EdgeInsets.all(10),
+            height: 70,
             child: LiquidLinearProgressIndicator(
-              value: double.parse(perc.toStringAsFixed(2)), // Defaults to 0.5.
-              valueColor: AlwaysStoppedAnimation(Colors.lightBlue
-                  .shade600), // Defaults to the current Theme's accentColor.
+              value: double.parse(perc.toStringAsFixed(5)), // Defaults to 0.5.
+              valueColor: AlwaysStoppedAnimation(
+                  triviaLevelPrimaryColor), // Defaults to the current Theme's accentColor.
               backgroundColor: Colors
-                  .white, // Defaults to the current Theme's backgroundColor.
-              borderColor: Colors.lightBlue.shade900, //border color of the bar
+                  .transparent, // Defaults to the current Theme's backgroundColor.
+              borderColor: triviaLevelDarkColor, //border color of the bar
               borderWidth: 5.0, //border width of the bar
               borderRadius: 12.0, //border radius
               direction: Axis.horizontal,
@@ -57,13 +72,12 @@ class TriviaLevelScreen extends GetView<LevelController> {
                   Text(
                     "${time.sec} seg",
                     style: TextStyle(
-                      color: Colors.lightBlue.shade900,
+                      color: Colors.white70,
                       fontSize: 20,
                     ),
                   ),
                   SvgPicture.asset(
                     "assets/icons/clock.svg",
-                    color: Colors.lightBlue.shade900,
                     width: 35,
                   ),
                 ],
@@ -75,26 +89,79 @@ class TriviaLevelScreen extends GetView<LevelController> {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: _appBar(),
-      body: _buildBody(),
+  _buildStepper() {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+          DotStepper(
+            dotCount: widget.subLevelController.dotCount,
+            dotRadius: 25,
+            activeStep: widget.subLevelController.activeStep,
+            shape: Shape.circle,
+            spacing: 10,
+            indicator: Indicator.jump,
+
+            /// TAPPING WILL NOT FUNCTION PROPERLY WITHOUT THIS PIECE OF CODE.
+            onDotTapped: (tappedDotIndex) {
+              widget.subLevelController.onDotTapped(tappedDotIndex);
+              setState(() {});
+            },
+
+            // DOT-STEPPER DECORATIONS
+            fixedDotDecoration: FixedDotDecoration(
+              color: triviaLevelPrimaryColor,
+            ),
+
+            indicatorDecoration: IndicatorDecoration(
+              color: triviaLevelSecundaryColor,
+            ),
+            lineConnectorDecoration: LineConnectorDecoration(
+              color: Colors.amber,
+              strokeWidth: 2,
+            ),
+          ),
+          SizedBox(height: 10),
+          _buildCountDown(),
+
+          //  Expanded(
+          //       child: PageView.builder(
+          //         // Block swipe to next qn
+          //         physics: const NeverScrollableScrollPhysics(),
+          //         controller: _questionController.pageController,
+          //         onPageChanged: _questionController.updateTheQnNum,
+          //         itemCount: _questionController.questions.length,
+          //         itemBuilder: (context, index) => QuestionCard(
+          //             question: _questionController.questions[index]),
+          //       ),
+          
+          Expanded(
+            child: FittedBox(
+              child: Center(
+                child: Text('${widget.subLevelController.activeStep}'),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
-  AppBar _appBar() {
-    return AppBar(
-      title: Text(
-        "Trivia Level 1",
-      ),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //  appBar: _appBar(),
+      body: _buildBody(),
     );
   }
 
   Widget _buildBody() {
     return Container(
-      child: Center(
-        child: _buildCountDown(),
+      child: Stack(
+        children: [
+          SvgPicture.asset("assets/icons/bg.svg", fit: BoxFit.fill,),
+          SafeArea(child: _buildStepper()),
+        ],
       ),
     );
   }
