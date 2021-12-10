9f1c00f0f6a9973ce43974dec71a971812c84b5c
Jessica Aidyl <jgarciaalbalah@gmail.com>
2021-12-09 20:41:41 -0500

    Level1: +correctAnswerId

--- lib/src/app/b_domain/levels/level1.dart
+++ lib/src/app/b_domain/levels/level1.dart
@@ -9,19 +9,19 @@ class Level1 {
       SubLevelDomain(
         id: 1,
         question: [
-          QuestionDomain(id: 1, question: "2 + 2 = ?", answers: [
+          QuestionDomain(id: 1,correctAnswerId: 2, question: "2 + 2 = ?", answers: [
             QuestionAnswerDomain(id: 1, answer: "2"),
             QuestionAnswerDomain(id: 2, answer: "4"),
             QuestionAnswerDomain(id: 3, answer: "6"),
             QuestionAnswerDomain(id: 4, answer: "8"),
           ]),
-          QuestionDomain(id: 2, question: "3 + 7 = ?", answers: [
+          QuestionDomain(id: 2,correctAnswerId: 3, question: "3 + 7 = ?", answers: [
             QuestionAnswerDomain(id: 1, answer: "8"),
             QuestionAnswerDomain(id: 2, answer: "20"),
             QuestionAnswerDomain(id: 3, answer: "10"),
             QuestionAnswerDomain(id: 4, answer: "Ninguna."),
           ]),
-          QuestionDomain(id: 3, question: "14 + 6 = ?", answers: [
+          QuestionDomain(id: 3,correctAnswerId: 1, question: "14 + 6 = ?", answers: [
             QuestionAnswerDomain(id: 1, answer: "20"),
             QuestionAnswerDomain(id: 2, answer: "40"),
             QuestionAnswerDomain(id: 3, answer: "60"),
