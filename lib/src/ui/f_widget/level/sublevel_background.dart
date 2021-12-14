9c893e4d2b006f053dd9747b12b5eded34eb1bb0
Jesus Hdez <jhernandezb96@gmail.com>
2021-12-13 22:47:24 -0500

    SubLevelBackground: +DecorationImage(real background)

--- lib/src/ui/f_widget/level/sublevel_background.dart
+++ lib/src/ui/f_widget/level/sublevel_background.dart
@@ -17,6 +17,7 @@ class _SubLevelBackgroundState extends State<SubLevelBackground> {
 
   @override
   void initState() {
+    super.initState();
     setState(
       () {
         _animatedWidget = SubLevelStartContdown(
@@ -24,7 +25,7 @@ class _SubLevelBackgroundState extends State<SubLevelBackground> {
             //really start the level
             setState(() {
               _animatedWidget =
-                  TriviaLevelScreen(subLevelDomain: widget.subLevelDomain);
+                  SubLevelScreen(subLevelDomain: widget.subLevelDomain);
             });
           },
         );
@@ -35,14 +36,18 @@ class _SubLevelBackgroundState extends State<SubLevelBackground> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        //TODO: background de verdad del subnivel, aqui va la foto de fondo
-        color: Colors.pink,
+      decoration: new BoxDecoration(
+        color: Colors.transparent, //pa si por si acaso
+        image: new DecorationImage(
+          //TODO: @Aidyl98 genial el uso de la imagen de fondo en el container, asi nos quitamos usar el stack para background de imagen, GENIAL!!!!
+          image: new AssetImage("assets/icons/background.jpg"),
+          fit: BoxFit.cover,
+        ),
       ),
       child: AnimatedSwitcher(
         duration: const Duration(milliseconds: 500),
         transitionBuilder: (Widget child, Animation<double> animation) {
-          /*return RotationTransition(
+          /*return RotationTransition(//TODO: @Aidyl98 elige que transición te gusta mas
             turns: animation,
             child: child,
           );*/
