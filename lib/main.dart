5e2f71ac1c9c6099ffdf46f969c462caa06df75f
Jesus Hdez <jhernandezb96@gmail.com>
2021-12-13 22:51:41 -0500

    main: title => 'Trivia Main'

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Meals Tutorial',
+      title: 'Trivia Main',
       //--------------------- <THEAMING> -----------------------------------
       darkTheme: ThemeData(
         primarySwatch: Colors.purple,
