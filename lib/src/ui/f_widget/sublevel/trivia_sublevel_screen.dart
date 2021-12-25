ecf5c1809304356749b375948fae88200cd3638e
Jesus Hdez <jhernandezb96@gmail.com>
2021-12-25 09:13:35 -0500

    sublevel_countdown & stepper: to _build

--- lib/src/ui/f_widget/sublevel/trivia_sublevel_screen.dart
+++ lib/src/ui/f_widget/sublevel/trivia_sublevel_screen.dart
@@ -1,7 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
 import 'package:citmatel_strawberry_trivia/trivia_exporter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_countdown_timer/index.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:im_stepper/stepper.dart';
+import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
 
 // ignore: must_be_immutable
 class TriviaSubLevelScreen extends GetView<TriviaSubLevelController> {
@@ -24,16 +27,129 @@ class TriviaSubLevelScreen extends GetView<TriviaSubLevelController> {
         return SafeArea(
           child: Column(children: [
             //Build the stepper.
-            TriviaSubLevelStepper(),
+            _buildStepper(),
             //Build the liquid progress bar.
             const SizedBox(height: 10),
-            TriviaSubLevelCountDown(),
+            _buildCountdown(),
             const SizedBox(height: 10),
             //Build the Question card
             Expanded(
               child: TriviaSubLevelQuestionCard(),
             ),
           ]),
+        );
+      },
+    );
+  }
+
+  _buildStepper() {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: DotStepper(
+        //Amount of dots to show.
+        dotCount: controller.dotCount,
+        //Size of the dots.
+        dotRadius: 25,
+        //Current selected dot.
+        activeStep: controller.activeStep,
+        //Type of shape of the dot.
+        shape: Shape.circle,
+        //Space between the dots.
+        spacing: 10,
+        //The animation that is shown when switch from a dot to another.
+        indicator: Indicator.jump,
+
+        //What should happen when a dot is tapped.
+        onDotTapped: controller.onDotTapped,
+
+        // DOT-STEPPER DECORATIONS
+        fixedDotDecoration: FixedDotDecoration(
+          color: Colors.indigoAccent.shade700,
+        ),
+
+        indicatorDecoration: IndicatorDecoration(
+          color: Colors.indigoAccent.shade100,
+        ),
+        lineConnectorDecoration: LineConnectorDecoration(
+          color: Colors.amber,
+          strokeWidth: 2,
+        ),
+      ),
+    );
+  }
+
+  _buildCountdown() {
+    //Current time.
+    int startTime = DateTime.now().millisecondsSinceEpoch;
+    //Seconds it takes to reach 0.
+    int dif = controller.durationOfProgressBar();
+    //The end time is the current time plus the choosen amount of seconds.
+    int endTime = startTime + dif * 1000;
+
+    CountdownTimerController _timerController = CountdownTimerController(
+        endTime: endTime,
+        onEnd: () {
+          //What should happen when time reach 0.
+          print("end game");
+        });
+
+    return CountdownTimer(
+      controller: _timerController,
+      widgetBuilder: (_, CurrentRemainingTime? time) {
+        if (time == null || time.sec == null) {
+          return Text('Game over');
+        }
+        //Current percent of the remaining time.
+        double perc = time.sec! / controller.durationOfProgressBar();
+
+        return Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.all(10),
+              height: 70,
+              child: LiquidLinearProgressIndicator(
+                //Value of the progress bar.
+                value: double.parse(perc.toStringAsFixed(5)),
+                //Color of the liquid animation.
+                valueColor:
+                    AlwaysStoppedAnimation(Colors.indigoAccent.shade700),
+                //Background Color of the progress bar.
+                backgroundColor: Colors.transparent,
+                //Border color of the bar.
+                borderColor: Colors.indigoAccent.shade100,
+                //Border width and radius of the bar.
+                borderWidth: 5.0,
+                borderRadius: 12.0,
+                //The direction the liquid moves.
+                //(Axis.vertical = bottom to top, Axis.horizontal = left to right).
+                //Defaults to Axis.horizontal.
+                direction: Axis.horizontal,
+                //Text inside bar.
+                //center: Text("${time.sec} sec"),
+              ),
+            ),
+            Positioned.fill(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "${time.sec} seg",
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 20,
+                      ),
+                    ),
+                    SvgPicture.asset(
+                      "assets/icons/clock.svg",
+                      width: 30,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         );
       },
     );
