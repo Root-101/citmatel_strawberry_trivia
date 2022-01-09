a784f50988ddcf8226c2bc2f225509ce1e9c2bb6
Aidyl <jgarciaalbalah@gmail.com>
2022-01-09 15:12:05 -0500

    TriviaSubLevelUseCase & Impl: access to domain clean

--- lib/src/app/c_usecase_def/trivia_sub_level_usecase.dart
+++ lib/src/app/c_usecase_def/trivia_sub_level_usecase.dart
@@ -1,5 +1,16 @@
+import 'package:citmatel_strawberry_trivia/trivia_exporter.dart';
 import 'package:clean_core/clean_core.dart';
+import 'package:flutter/material.dart';
 
 abstract class TriviaSubLevelUseCase extends AbstractUseCase {
-  
+  int get dotCount;
+
+  Map<QuestionState, Color> get colorMap;
+  Map<QuestionState, IconData> get iconsMap;
+
+  int correctAnswerId(int activeStep);
+  int durationOfProgressBar(int activeStep);
+  int questionsLength();
+
+  TriviaQuestionDomain currentQuestion(activeStep);
 }
--- lib/src/app/d_usecase_impl/trivia_sub_level_usecase_impl.dart
+++ lib/src/app/d_usecase_impl/trivia_sub_level_usecase_impl.dart
@@ -1,8 +1,50 @@
-import 'package:citmatel_strawberry_trivia/src/app/trivia_app_exporter.dart';
+import 'package:citmatel_strawberry_trivia/trivia_exporter.dart';
+import 'package:flutter/material.dart';
 
 class TriviaSubLevelUseCaseImpl extends TriviaSubLevelUseCase {
   // Domain almacenado para acceder a la info.
   final TriviaSubLevelDomain subLevelDomain;
 
-  TriviaSubLevelUseCaseImpl({required this.subLevelDomain}) {}
+  final Map<QuestionState, Color> _colorMap = {
+    QuestionState.Not_answered: kGrayColor,
+    QuestionState.Answered_rigth: kGreenColor,
+    QuestionState.Answered_wrong: kRedColor,
+  };
+
+  final Map<QuestionState, IconData> _iconsMap = {
+    QuestionState.Not_answered: Icons.circle_outlined,
+    QuestionState.Answered_rigth: Icons.done,
+    QuestionState.Answered_wrong: Icons.close,
+  };
+
+  TriviaSubLevelUseCaseImpl({required this.subLevelDomain});
+
+  @override
+  int get dotCount => subLevelDomain.question.length;
+
+  @override
+  Map<QuestionState, Color> get colorMap => this._colorMap;
+
+  @override
+  Map<QuestionState, IconData> get iconsMap => this._iconsMap;
+
+  @override
+  int durationOfProgressBar(int activeStep) {
+    return subLevelDomain.question[activeStep].duration;
+  }
+
+  @override
+  int correctAnswerId(int activeStep) {
+    return subLevelDomain.question[activeStep].correctAnswerId;
+  }
+
+  @override
+  TriviaQuestionDomain currentQuestion(activeStep) {
+    return subLevelDomain.question[activeStep];
+  }
+
+  @override
+  int questionsLength() {
+    return subLevelDomain.question.length;
+  }
 }
