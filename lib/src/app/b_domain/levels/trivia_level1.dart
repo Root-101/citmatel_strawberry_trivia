be291b4496c7b2d0a632661aa2332910e7fc45fd
Aidyl <jgarciaalbalah@gmail.com>
2022-01-09 18:59:37 -0500

    TriviaLevel1: change test values

--- lib/src/app/b_domain/levels/trivia_level1.dart
+++ lib/src/app/b_domain/levels/trivia_level1.dart
@@ -9,24 +9,36 @@ class TriviaLevel1 {
       TriviaSubLevelDomain(
         id: 1,
         question: [
-          TriviaQuestionDomain(id: 1,correctAnswerId: 2, question: "2 + 2 = ?", answers: [
-            TriviaQuestionAnswerDomain(id: 1, answer: "2"),
-            TriviaQuestionAnswerDomain(id: 2, answer: "4"),
-            TriviaQuestionAnswerDomain(id: 3, answer: "6"),
-            TriviaQuestionAnswerDomain(id: 4, answer: "8"),
-          ]),
-          TriviaQuestionDomain(id: 2,correctAnswerId: 3, question: "3 + 7 = ?", answers: [
-            TriviaQuestionAnswerDomain(id: 1, answer: "8"),
-            TriviaQuestionAnswerDomain(id: 2, answer: "20"),
-            TriviaQuestionAnswerDomain(id: 3, answer: "10"),
-            TriviaQuestionAnswerDomain(id: 4, answer: "Ninguna."),
-          ]),
-          TriviaQuestionDomain(id: 3,correctAnswerId: 1, question: "14 + 6 = ?", answers: [
-            TriviaQuestionAnswerDomain(id: 1, answer: "20"),
-            TriviaQuestionAnswerDomain(id: 2, answer: "40"),
-            TriviaQuestionAnswerDomain(id: 3, answer: "60"),
-            TriviaQuestionAnswerDomain(id: 4, answer: "80"),
-          ]),
+          TriviaQuestionDomain(
+              id: 1,
+              correctAnswerId: 2,
+              question: "Cómo hacer a una persona feliz?",
+              answers: [
+                TriviaQuestionAnswerDomain(id: 1, answer: "Dale comida."),
+                TriviaQuestionAnswerDomain(id: 2, answer: "Dale amor."),
+                TriviaQuestionAnswerDomain(id: 3, answer: "Dale libertad."),
+                TriviaQuestionAnswerDomain(id: 4, answer: "Dale con un palo."),
+              ]),
+          TriviaQuestionDomain(
+              id: 2,
+              correctAnswerId: 3,
+              question: "3 + 7 = ?",
+              answers: [
+                TriviaQuestionAnswerDomain(id: 1, answer: "8"),
+                TriviaQuestionAnswerDomain(id: 2, answer: "20"),
+                TriviaQuestionAnswerDomain(id: 3, answer: "10"),
+                TriviaQuestionAnswerDomain(id: 4, answer: "Ninguna."),
+              ]),
+          TriviaQuestionDomain(
+              id: 3,
+              correctAnswerId: 1,
+              question: "14 + 6 = ?",
+              answers: [
+                TriviaQuestionAnswerDomain(id: 1, answer: "20"),
+                TriviaQuestionAnswerDomain(id: 2, answer: "40"),
+                TriviaQuestionAnswerDomain(id: 3, answer: "60"),
+                TriviaQuestionAnswerDomain(id: 4, answer: "80"),
+              ]),
         ],
       ),
       TriviaSubLevelDomain(
