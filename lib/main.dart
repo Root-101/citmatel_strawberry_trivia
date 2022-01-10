3afef32207ff5927180c875f1bbeea202025b2b6
Aidyl <jgarciaalbalah@gmail.com>
2022-01-09 19:20:02 -0500

    main: +StrawberryLevelWin & Lose

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:citmatel_strawberry_tools/tools_exporter.dart';
 import 'package:citmatel_strawberry_trivia/src/ui/trivia_ui_exporter.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -25,6 +26,16 @@ class MyApp extends StatelessWidget {
           name: TriviaLevelsScreen.ROUTE_NAME,
           page: () => TriviaLevelsScreen(),
           transition: Transition.rightToLeft,
+        ),
+        GetPage(
+          name: StrawberryLevelWin.ROUTE_NAME,
+          page: () => StrawberryLevelWin(),
+          transition: Transition.rightToLeft,
+        ),
+        GetPage(
+          name: StrawberryLevelLose.ROUTE_NAME,
+          page: () => StrawberryLevelLose(),
+          transition: Transition.leftToRightWithFade,
         ),
       ],
       //--------------------- </PAGINATION> -----------------------------------
