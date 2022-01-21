252cad812bc05881cf3e2f8aad9051b397d81494
JHB <jhernandezb96@gmail.com>
2022-01-21 11:59:57 -0500

    +domain/progress

---
+++ lib/src/app/b_domain/progress/trivia_progress_exporter.dart
@@ -0,0 +1,4 @@
+library trivia_progress_exporter;
+
+//exporta el domain de progreso
+export 'trivia_sublevel_progress_domain.dart';
--- lib/src/app/b_domain/trivia_domain_exporter.dart
+++ lib/src/app/b_domain/trivia_domain_exporter.dart
@@ -1,10 +1,9 @@
 library trivia_domain_exporter;
 
+export 'levels/trivia_level_exporter.dart';
+export 'progress/trivia_progress_exporter.dart';
 //exporta los domain
 export 'trivia_level_domain.dart';
 export 'trivia_question_answer_domain.dart';
 export 'trivia_question_domain.dart';
 export 'trivia_sub_level_domain.dart';
-
-
-export 'levels/trivia_level_exporter.dart';
