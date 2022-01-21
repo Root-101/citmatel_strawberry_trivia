cf296951d76a4d8157cfe34955445e503c478e37
JHB <jhernandezb96@gmail.com>
2022-01-21 12:11:38 -0500

    main.dart: loading main

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
-import 'package:citmatel_strawberry_trivia/src/ui/trivia_ui_exporter.dart';
+import 'package:citmatel_strawberry_trivia/trivia_exporter.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() {
-  TriviaUIModule.init();
-  runApp(MyApp());
+  runApp(SplashScreen());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Trivia Main',
+      title: 'Trivia Tutorial',
       //--------------------- <THEAMING> -----------------------------------
       darkTheme: ThemeData(
         primarySwatch: Colors.purple,
@@ -23,11 +22,47 @@ class MyApp extends StatelessWidget {
       getPages: [
         GetPage(
           name: TriviaLevelsScreen.ROUTE_NAME,
-          page: () => TriviaLevelsScreen(),
+          page: () =>
+              //entrada directo al nivel 1
+              /*DnDSubLevelScreen(
+                  subLevelDomain: DnDLevel1.level1.sublevel[0]),*/
+              //entrada a el grid de seleccion de niveles
+              TriviaLevelsScreen(),
           transition: Transition.rightToLeft,
         ),
       ],
+      /*unknownRoute: GetPage(
+          name: UnknownRouteScreen.ROUTE_NAME,
+          page: () => UnknownRouteScreen()),*/
       //--------------------- </PAGINATION> -----------------------------------
     );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Init.instance.initialize(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else {
+          return MyApp();
+        }
+      },
+    );
+  }
+}
+
+class Init {
+  Init._();
+
+  static final instance = Init._();
+
+  Future initialize() async {
+    await TriviaUIModule.init();
   }
 }
