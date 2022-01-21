51c777731edb2ec94737e5a577207921a571fd34
JHB <jhernandezb96@gmail.com>
2022-01-21 12:00:54 -0500

    +app/usecase_impl

---
+++ lib/src/app/d_usecase_impl/trivia_sublevel_progress_usecase_impl.dart
@@ -0,0 +1,39 @@
+import 'package:citmatel_strawberry_trivia/src/app/trivia_app_exporter.dart';
+import 'package:clean_core/clean_core.dart';
+
+class TriviaSubLevelProgressUseCaseImpl
+    extends DefaultCRUDUseCase<TriviaSubLevelProgressDomain>
+    implements TriviaSubLevelProgressUseCase {
+  late TriviaSubLevelProgressRepo _repo;
+
+  TriviaSubLevelProgressUseCaseImpl(TriviaSubLevelProgressRepo repo)
+      : _repo = repo,
+        super(repo: repo);
+
+  @override
+  List<TriviaSubLevelProgressDomain> findByLevel(TriviaLevelDomain level) =>
+      this.findByLevelId(level.id);
+
+  @override
+  List<TriviaSubLevelProgressDomain> findByLevelId(int levelId) =>
+      _repo.findByLevelId(levelId);
+
+  @override
+  TriviaSubLevelProgressDomain findByAll(
+          TriviaLevelDomain level, TriviaSubLevelDomain subLevel) =>
+      this.findByAllId(level.id, subLevel.id);
+
+  @override
+  TriviaSubLevelProgressDomain findByAllId(int levelId, int subLevelId) {
+    TriviaSubLevelProgressDomain? domain =
+        _repo.findByAllId(levelId, subLevelId);
+    // si es null devuelvo uno vacio para mostrarlo sin progreso en el main menu
+    // si tiene algo lo devuelvo
+    return domain == null
+        ? TriviaSubLevelProgressDomain(
+            triviaLevelDomainId: levelId,
+            triviaSubLevelDomainId: subLevelId,
+          )
+        : domain;
+  }
+}
--- lib/src/app/d_usecase_impl/trivia_usecase_impl_exporter.dart
+++ lib/src/app/d_usecase_impl/trivia_usecase_impl_exporter.dart
@@ -3,3 +3,4 @@ library trivia_usecase_impl_exporter;
 //exporta los usecase impl
 export 'trivia_level_usecase_impl.dart';
 export 'trivia_sub_level_usecase_impl.dart';
+export 'trivia_sublevel_progress_usecase_impl.dart';
