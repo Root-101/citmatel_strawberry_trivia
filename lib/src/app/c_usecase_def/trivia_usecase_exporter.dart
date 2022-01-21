fbe3fa653b01c99b164d78b805fa1feb381d0c47
JHB <jhernandezb96@gmail.com>
2022-01-21 12:00:09 -0500

    +app/usecase_def

--- lib/src/app/c_usecase_def/trivia_usecase_exporter.dart
+++ lib/src/app/c_usecase_def/trivia_usecase_exporter.dart
@@ -3,3 +3,4 @@ library trivia_usecase_exporter;
 //exporta los usecase def
 export 'trivia_level_usecase.dart';
 export 'trivia_sub_level_usecase.dart';
+export 'trivia_sublevel_progress_usecase.dart';
