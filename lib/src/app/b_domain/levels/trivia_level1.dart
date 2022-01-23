7af3ee470f80cea633a9b5d1f453d43841ee55ad
JHB <jhernandezb96@gmail.com>
2022-01-23 16:34:01 -0500

    TriviaLevel1: +urlThemePicture

--- lib/src/app/b_domain/levels/trivia_level1.dart
+++ lib/src/app/b_domain/levels/trivia_level1.dart
@@ -1,10 +1,11 @@
+import 'package:citmatel_strawberry_trivia/assets/trivia_assets.dart';
 import 'package:citmatel_strawberry_trivia/src/app/trivia_app_exporter.dart';
 
 class TriviaLevel1 {
   static final TriviaLevelDomain level1 = TriviaLevelDomain(
     id: 1,
     theme: "Tutorial",
-    urlThemePicture: "",
+    urlThemePicture: TriviaAssets.WALLPAPER,
     sublevel: [
       TriviaSubLevelDomain(
         id: 1,
