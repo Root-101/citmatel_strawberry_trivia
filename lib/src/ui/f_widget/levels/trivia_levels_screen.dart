20582625ec12d5c13e32989e84116971f165f32e
JHB <jhernandezb96@gmail.com>
2022-01-23 16:34:19 -0500

    TriviaLevelsScreen: to CommonsLevelsScreen

--- lib/src/ui/f_widget/levels/trivia_levels_screen.dart
+++ lib/src/ui/f_widget/levels/trivia_levels_screen.dart
@@ -1,97 +1,39 @@
+import 'package:citmatel_strawberry_tools/tools_exporter.dart';
 import 'package:citmatel_strawberry_trivia/trivia_exporter.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:page_view_indicators/page_view_indicators.dart';
 
 class TriviaLevelsScreen extends GetView<TriviaLevelController> {
   static const ROUTE_NAME = "/trivia-levels-screen";
 
   TriviaLevelsScreen({Key? key}) : super(key: key);
 
-  //for page view and indicators
-  final _pageController = PageController();
-  final _currentPageNotifier = ValueNotifier<int>(0);
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: new BoxDecoration(
-          color: Colors
-              .white, //default color, sobre este se pone la imagen correspondiente a cada nivel
-        ),
-        child: ArrowPageIndicator(
-          iconSize: 25,
-          isInside: true,
-          pageController: _pageController,
-          currentPageNotifier: _currentPageNotifier,
-          itemCount: controller.count(),
-          child: Stack(
-            children: <Widget>[
-              _buildPageView(),
-              _buildCircleIndicator(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  _buildPageView() {
-    return Container(
-      child: PageView.builder(
-        itemCount: controller.count(),
-        controller: _pageController,
-        itemBuilder: (BuildContext context, int index) {
-          return _buildLevelGridView(controller.findAll()[index]);
-        },
-        onPageChanged: (int index) {
-          _currentPageNotifier.value = index;
-        },
-      ),
-    );
-  }
-
-  _buildCircleIndicator() {
-    return Positioned(
-      left: 0.0,
-      right: 0.0,
-      bottom: 0.0,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: CirclePageIndicator(
-          size: 10,
-          selectedSize: 12,
-          itemCount: controller.count(),
-          currentPageNotifier: _currentPageNotifier,
-        ),
-      ),
-    );
-  }
-
-  _buildLevelGridView(TriviaLevelDomain level) {
-    //con un GetBuilder para que actualize el progreso cuando se gane un nivel
-    return GridView(
-      physics: BouncingScrollPhysics(),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-      ),
-      children: level.sublevel
-          .map(
-            (subLevel) => GetBuilder<TriviaLevelController>(
-              builder: (context) {
-                return TriviaSingleLevelTile(
-                  subLevelDomain: subLevel,
-                  subLevelProgressDomain:
-                      Get.find<TriviaSubLevelProgressUseCase>().findByAll(
-                    level,
-                    subLevel,
-                  ),
-                );
-              },
-            ),
-          )
-          .toList(),
+    return CommonsLevelsScreen<TriviaLevelDomain>(
+      levelsFindAll: controller.findAll(),
+      buildSingleLevel: (levelDomain) {
+        return CommonsSingleLevel<TriviaSubLevelDomain>(
+          urlThemePicture: levelDomain.urlThemePicture,
+          subLevelsAll: (levelDomain).sublevel,
+          singleLevelBuilder: (subLevelDomain) {
+            return GetBuilder<TriviaLevelController>(builder: (context) {
+              TriviaSubLevelProgressDomain progressDomain =
+                  Get.find<TriviaSubLevelProgressUseCase>().findByAll(
+                levelDomain,
+                subLevelDomain,
+              );
+              return CommonsSingleSubLevelTile(
+                  stars: progressDomain.stars,
+                  contPlayedTimes: progressDomain.contPlayedTimes,
+                  openWidget: TriviaSubLevelBackground(
+                    subLevelDomain: subLevelDomain,
+                    subLevelProgressDomain: progressDomain,
+                  ));
+            });
+          },
+        );
+      },
     );
   }
 }
