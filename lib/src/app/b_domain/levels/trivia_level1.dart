aa66e9ed02b7dc5de99f0cbbbeac74c4abd98210
JHB <jhernandezb96@gmail.com>
2022-01-27 04:42:12 -0500

    Migrado a TriviaLevelsScreen

--- lib/src/app/b_domain/levels/trivia_level1.dart
+++ lib/src/app/b_domain/levels/trivia_level1.dart
@@ -1,11 +1,17 @@
-import 'package:citmatel_strawberry_trivia/assets/trivia_assets.dart';
+import 'dart:ui';
+
+import 'package:citmatel_strawberry_tools/tools_exporter.dart';
 import 'package:citmatel_strawberry_trivia/src/app/trivia_app_exporter.dart';
 
 class TriviaLevel1 {
   static final TriviaLevelDomain level1 = TriviaLevelDomain(
     id: 1,
     theme: "Tutorial",
-    urlThemePicture: TriviaAssets.WALLPAPER,
+    themeBackgroundImage: ToolsThemesBackgroundImage(
+      urlImage: ToolsThemesAssets.THEME_CULTURA_GENERAL,
+      colorStrong: Color(0xffd4801c),
+      colorLight: Color(0xffe1a41b).withOpacity(0.5),
+    ),
     sublevel: [
       TriviaSubLevelDomain(
         id: 1,
