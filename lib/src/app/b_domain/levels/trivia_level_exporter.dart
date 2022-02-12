de9a4e9e05b8cfc50d8a6649ffba0a5575930183
Aidyl <jgarciaalbalah@gmail.com>
2022-02-12 18:27:07 -0500

    TriviaLevelsAll: +TriviaLevelGeography

--- lib/src/app/b_domain/levels/trivia_level_exporter.dart
+++ lib/src/app/b_domain/levels/trivia_level_exporter.dart
@@ -5,4 +5,6 @@ export 'trivia_level_0_tutorial.dart';
 export 'trivia_level_1_conocimiento_general.dart';
 export 'trivia_level_2_ciencias_naturales.dart';
 export 'trivia_level_3_tecnologias.dart';
+export 'trivia_level_4_mitology.dart';
+export 'trivia_level_6_geography.dart';
 export 'trivia_levels_all.dart';
